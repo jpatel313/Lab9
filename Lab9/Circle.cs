e9f42824146a74f12c997d4899565f44af1aa39f
Jay Patel <jcp313@gmail.com>
2016-11-04 14:47:05 -0400

    initial

---
+++ Lab9/Circle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Circle
+    {
+        private double radius;
+
+        #region Methods
+        
+        public double Radius
+        {
+            get
+            { return radius; }
+
+            set
+            { radius = value; }
+        }
+
+        public Circle(double x)
+        {
+            //this.radius= radius; //alt way turns user input into local variable
+            radius = x;
+        }
+
+        public double getCircumference()
+        {
+            double circumference = radius * (2 * Math.PI);
+            return circumference;
+        }
+
+        public double getArea()
+        {
+            double area = (radius * radius)* Math.PI;
+            return area;
+        }
+
+        private String formatNumber(double x)
+        {
+            return Math.Round(x, 2).ToString();
+        }
+
+        public String getFormattedCircumference()
+        {
+          return  formatNumber(getCircumference());
+        }
+
+        public String getFormattedArea()
+        {
+            return formatNumber(getArea());
+        }
+        #endregion
+    }
+}
---
+++ Lab9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class CircleApp
+    {
+        static void Main(string[] args)
+        {
+            //get input
+            Console.WriteLine("Welcome to the Circle Tester\n Enter radius: ");
+            double x = double.Parse(Console.ReadLine());
+
+            //create circle object
+            Circle  userCircle = new Circle(x);
+            
+            //display circumference/area
+            {
+               return userCircle.getFormattedCircumference(x);
+                return formatNumber(getCircumference(x));
+                Console.WriteLine("\nCircumference: {0}\nArea:         {1}", formatNumber(getFormattedCircumference(x), return formatNumber(getArea(x)); ;
+            }
+
+
+        }
+
+
+
+    }
+
+}
